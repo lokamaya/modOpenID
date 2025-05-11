114a6f5a65be8ddb9d80bcc87a25e3d25d0d4340
Zaenal <zaenal.muttaqin@gmail.com>
2025-05-11 20:28:48 +0700

    System Setting updated
    System setting build and lexicon

--- core/components/modopenid/lexicon/en/default.inc.php
+++ core/components/modopenid/lexicon/en/default.inc.php
@@ -1,4 +1,25 @@
 <?php
 
 $_lang['modopenid.menu'] = 'modOpenID';
+$_lang['modopenid.menu_desc'] = 'OpenID for MODX';
 $_lang['modopenid.manage.page_title'] = 'modOpenID';
+
+//System Setting
+$_lang['setting_modopenid.client_id'] = 'Client ID';
+$_lang['setting_modopenid.client_id_desc'] = 'Client ID provided by Identity Provider';
+$_lang['setting_modopenid.client_secret'] = 'Client Secret';
+$_lang['setting_modopenid.client_secret_desc'] = 'Client Secret provided by Identity Provider';
+$_lang['setting_modopenid.provider_url'] = 'Provider URL';
+$_lang['setting_modopenid.provider_url_desc'] = 'URL for OpenID connection provided by Identity Provider';
+$_lang['setting_modopenid.http_proxy'] = 'HTTP Proxy';
+$_lang['setting_modopenid.http_proxy_desc'] = 'Scopes used to get detail of current user (see: https://auth0.com/docs/get-started/apis/scopes/openid-connect-scopes)';
+$_lang['setting_modopenid.verify_host'] = 'Verify Host';
+$_lang['setting_modopenid.verify_host_desc'] = 'Enable verify host (default:true), only disable on development stage';
+$_lang['setting_modopenid.verify_peer'] = 'Verify Peer';
+$_lang['setting_modopenid.verify_peer_desc'] = 'Enable verify peer (default:true), only disable on development stage';
+$_lang['setting_modopenid.scopes'] = 'Scope';
+$_lang['setting_modopenid.scopes_desc'] = 'Scopes used to get detail of current user (see: https://auth0.com/docs/get-started/apis/scopes/openid-connect-scopes)';
+$_lang['setting_modopenid.certpath'] = 'Path to Certificate';
+$_lang['setting_modopenid.certpath_desc'] = 'Path to my certificate stored on local server';
+$_lang['setting_modopenid.debug'] = 'Debug OpenID';
+$_lang['setting_modopenid.certpath_desc'] = 'Enable/disable debuging (default:false)';
